885cc80049646ee49e0683ca382f85c00da3382c
[Neeraj kushwaha] <[workwithneeraj.01@gmail.com]>
2026-04-07 14:49:16 +0530

    added height

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
                     Elevated Hair & Beauty Experience
                 </h1>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 w-full px-2 sm:px-4 max-w-md sm:max-w-2xl">
-                    <div className="bg-white/10 text-white px-2 sm:px-4 md:px-6 py-4 sm:py-6 md:py-10 rounded-sm transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl group border border-white/30 flex flex-col items-center justify-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 w-full px-2 sm:px-4 max-w-xs sm:max-w-md md:max-w-2xl">
+                    <div className="bg-white/10 text-white px-2 sm:px-4 md:px-6 py-6 sm:py-6 md:py-10 rounded-sm transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl group border border-white/30 flex flex-col items-center justify-center">
                         <p className="font-sans text-[10px] sm:text-xs md:text-sm uppercase tracking-[0.12em] text-white/60 mb-2 md:mb-3">Book appointment</p>
                         <h3 className="font-serif text-xl sm:text-2xl md:text-4xl text-white mb-3 md:mb-6 font-light">Tauranga</h3>
                         <a
@@ -23,7 +23,7 @@ export default function Hero() {
                             Book Now
                         </a>
                     </div>
-                    <div className="bg-white/10 text-white px-2 sm:px-4 md:px-6 py-4 sm:py-6 md:py-10 rounded-sm transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl group border border-white/30 flex flex-col items-center justify-center">
+                    <div className="bg-white/10 text-white px-2 sm:px-4 md:px-6 py-6 sm:py-6 md:py-10 rounded-sm transition-all duration-300 hover:-translate-y-2 hover:shadow-2xl group border border-white/30 flex flex-col items-center justify-center">
                         <p className="font-sans text-[10px] sm:text-xs md:text-sm uppercase tracking-[0.12em] text-white/60 mb-2 md:mb-3">Book appointment</p>
                         <h3 className="font-serif text-xl sm:text-2xl md:text-4xl text-white mb-3 md:mb-6 font-light">Thames</h3>
                         <a
